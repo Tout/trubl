fdac7b216d5a74cfeefaa03e2184cb81494fe27e
Kristine Robison <kris@tout.com>
2013-01-22 13:38:40 -0800

    bumping version for gem reload

--- lib/retout/version.rb
+++ lib/retout/version.rb
@@ -2,7 +2,7 @@ module ReTout
   class Version
     MAJOR = 1 unless defined? ReTout::Version::MAJOR
     MINOR = 0 unless defined? ReTout::Version::MINOR
-    PATCH = 0 unless defined? ReTout::Version::PATCH
+    PATCH = 1 unless defined? ReTout::Version::PATCH
 
     class << self
 
