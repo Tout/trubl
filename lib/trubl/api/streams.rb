03d73a649a60a1058211d4fd6b0c9be5ae372a8c
Kristine Robison <kris@tout.com>
2013-01-24 16:56:12 -0800

    fixed old reference to ReTout

--- lib/trubl/api/streams.rb
+++ lib/trubl/api/streams.rb
@@ -6,7 +6,7 @@ module Trubl
 
       def retrieve_stream_touts(uid, order=nil, per_page=nil, page=nil)
         response = get("streams/#{uid}/touts", query: {order: order, per_page: per_page, page: page})
-        ReTout::Touts.new.from_response(response)
+        Trubl::Touts.new.from_response(response)
       end
 
     end
