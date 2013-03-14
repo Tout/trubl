5749fbb5ff4ccb465154cf822c872314815ffd95
Aaron Glenn <aaron@tout.com>
2013-03-14 14:04:44 -0700

    Pin version of floating deps. Bump version for hashtag follow/unfollow support.

--- lib/trubl/version.rb
+++ lib/trubl/version.rb
@@ -1,7 +1,7 @@
 module Trubl
   class Version
     MAJOR = 1 unless defined? Trubl::Version::MAJOR
-    MINOR = 2 unless defined? Trubl::Version::MINOR
+    MINOR = 3 unless defined? Trubl::Version::MINOR
     PATCH = 0 unless defined? Trubl::Version::PATCH
 
     class << self
