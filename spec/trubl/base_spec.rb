7bbe74dfe089b5abb82ede0739d77c6ea3283432
Aaron Glenn <aaron@tout.com>
2013-03-22 13:26:41 -0700

    Return nil when response code is 4xx or 5xx.

--- spec/trubl/base_spec.rb
+++ spec/trubl/base_spec.rb
@@ -11,9 +11,20 @@ describe Trubl::Base do
     resp
   }
 
-  #it "" do
-    #response.body.should == 'foo'
-  #end
+  let(:missing_response){ 
+    resp = double("Response")
+    resp.stub(:body).and_return('{"error": "resource not found"}') 
+    resp.stub(:code).and_return(404) 
+    resp
+  }
+
+  let(:error_response){ 
+    resp = double("Response")
+    resp.stub(:body).and_return('{"error": "kaboom"}') 
+    resp.stub(:code).and_return(500) 
+    resp
+  }
+
 
   it "inherits from Hash" do
     subject.is_a? Hash
@@ -25,18 +36,32 @@ describe Trubl::Base do
 
   describe "#from_response" do
 
+    it "returns nil if response code is 4xx" do
+      missing_response.should_receive(:code)
+      instance = subject.from_response(missing_response)
+      instance.should be_nil
+    end
+
+    it "returns nil if response code is 5xx" do
+      error_response.should_receive(:code)
+      instance = subject.from_response(error_response)
+      instance.should be_nil
+    end
 
     it "parses the response body" do
+      response.should_receive(:code)
       subject.should_receive(:parse).with(response)
       subject.from_response(response)
     end
 
     it "uses the inferred class name" do
+      response.should_receive(:code)
       subject.should_receive(:klass_name)
       subject.from_response(response)
     end
 
     it "re-initializes the instance" do
+      response.should_receive(:code)
       subject.should_receive :initialize
       subject.from_response(response)
     end
