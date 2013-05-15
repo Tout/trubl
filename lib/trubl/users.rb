c6e126b337e650f4f617246e8d3e96ff970f55ca
Aaron Glenn <aaron@aaronglenn.ca>
2013-05-15 16:58:52 -0700

    Remove explicit overrides now that the collection base class properly infers the klass, container_name and member_name.

--- lib/trubl/users.rb
+++ lib/trubl/users.rb
@@ -4,17 +4,5 @@ require_relative './user'
 module Trubl
   class Users < Collection
 
-    def klass
-      Trubl::User
-    end
-
-    def container_name
-      'users'
-    end
-
-    def member_name
-      'user'
-    end
-
   end
 end
