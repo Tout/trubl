b16eb380403f9d49ccd063b142a36ee18e9b3de0
Aaron Glenn <aaron@aaronglenn.ca>
2013-10-02 12:37:19 -0700

    Adding create stream endpoint and a sample response.

--- lib/trubl/api/streams.rb
+++ lib/trubl/api/streams.rb
@@ -4,6 +4,11 @@ module Trubl
   module API
     module Streams
 
+      def create_stream(logic = {})
+        response = post("streams/", body: logic)
+        Trubl::Stream.new.from_response(response)
+      end
+
       def retrieve_stream_touts(uid, order=nil, per_page=nil, page=nil)
         response = get("streams/#{uid}/touts", query: {order: order, per_page: per_page, page: page})
         Trubl::Touts.new.from_response(response)
@@ -16,4 +21,5 @@ module Trubl
 
     end
   end
+
 end
