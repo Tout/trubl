8f9c67106a01ab556db45ca1cf170d2686841a39
Javid Jamae <javidjamae@gmail.com>
2015-03-30 10:10:03 -0700

    Updated yajl-ruby version to 1.2.1

--- lib/trubl/version.rb
+++ lib/trubl/version.rb
@@ -1,8 +1,8 @@
 module Trubl
   class Version
-    MAJOR = 1 unless defined? Trubl::Version::MAJOR
-    MINOR = 5 unless defined? Trubl::Version::MINOR
-    PATCH = 9 unless defined? Trubl::Version::PATCH
+    MAJOR = 1  unless defined? Trubl::Version::MAJOR
+    MINOR = 5  unless defined? Trubl::Version::MINOR
+    PATCH = 10 unless defined? Trubl::Version::PATCH
 
     class << self
 
