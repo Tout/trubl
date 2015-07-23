197d0fc15ec0e561eac4950305f3b0dbdcf94a82
Will Bryant <william@tout.com>
2015-07-22 19:31:32 -0700

    bump version

--- lib/trubl/version.rb
+++ lib/trubl/version.rb
@@ -2,7 +2,7 @@ module Trubl
   class Version
     MAJOR = 1  unless defined? Trubl::Version::MAJOR
     MINOR = 6  unless defined? Trubl::Version::MINOR
-    PATCH = 1 unless defined? Trubl::Version::PATCH
+    PATCH = 2 unless defined? Trubl::Version::PATCH
 
     class << self
 
