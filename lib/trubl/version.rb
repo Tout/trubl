3bab3295f935dd75a42f3181f628c436eb2c24f5
Roman Gurovich <romangurovich@gmail.com>
2018-06-19 18:35:20 -0700

    PLAT-3166 Bump version

--- lib/trubl/version.rb
+++ lib/trubl/version.rb
@@ -2,7 +2,7 @@ module Trubl
   class Version
     MAJOR = 2  unless defined? Trubl::Version::MAJOR
     MINOR = 0  unless defined? Trubl::Version::MINOR
-    PATCH = 4  unless defined? Trubl::Version::PATCH
+    PATCH = 5  unless defined? Trubl::Version::PATCH
 
     class << self
 
